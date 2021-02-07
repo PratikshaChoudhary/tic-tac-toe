3c9c8212ec58a39a51bb2b2ff588cc32c91c0944
Pratiksha Choudhary <choudharypratiksha007@gmail.com>
2021-02-07 05:00:00 -0700

    adding code for user to play

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -4,6 +4,21 @@
 
 char board[3][3];
 
+char choose_ran(){
+  return ((rand() % 10) > 5 ?  'X' : 'O');
+}
+
+
+void initilize_board(){
+
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            board[i][j]=' ';
+        }
+    }
+}
+
+
 void print_board(){
 
     printf("\n\n"); 
@@ -14,12 +29,40 @@ void print_board(){
 } 
 
 
-void initilize_board(){
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            board[i][j]=' ';
+void play(char user, char comp){
+  
+    char status;
+    char str[5];
+    int r, c;
+
+
+    for(int i = 1; i < 9; i++){
+     
+        again:
+        printf("tictactoe>");
+        scanf("%s",str);
+
+        switch(str[0]){
+            case 'm':    
+                scanf("%d , %d",&r, &c);
+                printf("%c   %d   %d",str[0],r,c);
+
+                if(board[r][c] == ' '){
+                    board[r][c] = user;
+                  
+                }
+                else{
+                    printf("Invalid move, place already taken\n");
+                    goto again;
+                }
+                break;
+            case 'q':
+                return 'c';
+
         }
-    }
+        return ' ';
+    }     
+    
 }
 
 
@@ -27,8 +70,13 @@ int main(){
 
     char user, comp;
 
+    srand(time(0));
+    user = choose_ran();
+    comp = (user == 'X' ? 'O' : 'X');
+
     initilize_board();
     print_board();
+    play(user, comp);
 
     return 0;
 }
