9ac3c71fd28428d57101c47ae76503daf13128fc
Pratiksha Choudhary <choudharypratiksha007@gmail.com>
2021-02-08 07:00:00 -0700

    Adding computer play as well

--- tic_tac_toe.c
+++ tic_tac_toe.c
@@ -1,23 +1,9 @@
+
 #include<stdio.h>
 #include <stdlib.h>
-
+#include<time.h>
 
 char board[3][3];
-
-char choose_ran(){
-  return ((rand() % 10) > 5 ?  'X' : 'O');
-}
-
-
-void initilize_board(){
-
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            board[i][j]=' ';
-        }
-    }
-}
-
 
 void print_board(){
 
@@ -29,40 +15,131 @@ void print_board(){
 } 
 
 
-void play(char user, char comp){
-  
-    char status;
+char choose_ran(){
+  return ((rand() % 10) > 5 ?  'X' : 'O');
+}
+
+int check(int r, int c, char v){
+
+    if(board[r][0] ==v  && board[r][1] == v &&  board[r][2] == v)
+        return 1;
+    if(board[0][c] == v && board[1][c] == v &&  board[2][c] == v)
+        return 1;
+    if((r+c)%2 == 0){
+        if( r == 1  && c == 1){
+            if(board[0][0] ==v  && board[1][1] == v &&  board[2][2] == v)
+                return 1;
+
+            if(board[2][0] ==v  && board[1][1] == v &&  board[0][2] == v)
+                return 1;
+        }
+        else{
+            if(r == c){
+                if(board[0][0] ==v  && board[1][1] == v &&  board[2][2] == v)
+                    return 1;
+                else {
+                    if(board[2][0] ==v  && board[1][1] == v &&  board[0][2] == v)
+                        return 1;
+                }
+
+            }
+        }
+
+    }
+return 0;
+}
+
+
+char *comp_turn(int comp,int i){
+    int r, c;
+    again:
+    r = rand()%3;
+    c = rand()%3;
+    if(board[r][c] == ' '){
+        board[r][c] = comp;
+            if(i>2){
+                if(check(r,c,board[r][c]) == 1){
+                    return 'c';                        
+                }
+            }
+        }
+        else{
+            printf("Invalid move, place already taken\n");
+            goto again;
+        }
+    return ' ';
+}
+
+
+char *user_turn(int user, int i){
     char str[5];
     int r, c;
 
+    again:
+    printf("tictactoe>");
+    scanf("%s",str);
+
+    switch(str[0]){
+        case 'm':    
+            scanf("%d , %d",&r, &c);
+            printf("%c   %d   %d",str[0],r,c);
+
+            if(board[r][c] == ' '){
+                board[r][c] = user;
+                if(i>2){
+                    if(check(r,c,board[r][c]) == 1){
+                        return 'u';                        
+                    }
+                }
+            }
+            else{
+                printf("Invalid move, place already taken\n");
+                goto again;
+            }
+            break;
+        case 'q':
+            return 'c';
+
+    }
+    return ' ';
+}
+
+
+void play(char user, char comp){
+  
+    char status;
 
     for(int i = 1; i < 9; i++){
-     
-        again:
-        printf("tictactoe>");
-        scanf("%s",str);
-
-        switch(str[0]){
-            case 'm':    
-                scanf("%d , %d",&r, &c);
-                printf("%c   %d   %d",str[0],r,c);
-
-                if(board[r][c] == ' '){
-                    board[r][c] = user;
-                  
-                }
-                else{
-                    printf("Invalid move, place already taken\n");
-                    goto again;
-                }
-                break;
-            case 'q':
-                return 'c';
-
+        if(i%2 != 0){
+            if(user == 'X')
+            status = user_turn(user, i);
+            else
+                comp_turn(comp, i);
+        }else {
+            if(user == 'O')
+                status = user_turn(user, i);
+            else 
+                comp_turn(comp, i);
         }
-        return ' ';
+        print_board(i);
+        if(status == 'u'){
+            printf("User wins");
+            return ;
+        }
+        else if(status == 'c'){
+            printf("Computer Wins");
+            return ;
+        }
     }     
-    
+}
+
+
+void initilize_board(){
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            board[i][j]=' ';
+        }
+    }
 }
 
 
@@ -73,9 +150,8 @@ int main(){
     srand(time(0));
     user = choose_ran();
     comp = (user == 'X' ? 'O' : 'X');
-
     initilize_board();
-    print_board();
+    printf("%c %c\n",user,comp);
     play(user, comp);
 
     return 0;
